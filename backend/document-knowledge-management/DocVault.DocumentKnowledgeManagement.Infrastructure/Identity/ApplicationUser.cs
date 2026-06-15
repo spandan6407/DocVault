418fad1d90342f4a116de112ff0986868a3a43a2
Spandan.Swain <spandanswain17@gmail.com>
2026-06-15 11:10:01 +0530

    feature/ss/identity-setup: add ApplicationUser and ApplicationDbContext

---
+++ backend/document-knowledge-management/DocVault.DocumentKnowledgeManagement.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocVault.DocumentKnowledgeManagement.Infrastructure.Identity
+{
+    internal class ApplicationUser
+    {
+    }
+}
